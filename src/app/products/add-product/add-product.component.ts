30ac3b24ea2f8577f0495e9cd48d2141b1c77ef7
Umair Riaz <lib.umair@gmail.com>
2021-08-08 09:31:45 -0700

    updated code 8-Aug-2021 9:31PM

--- src/app/products/add-product/add-product.component.ts
+++ src/app/products/add-product/add-product.component.ts
@@ -11,24 +11,38 @@ export class AddProductComponent implements OnInit {
   
   constructor(private proService:ProductsService, private http: HttpClient ) { }
   productData:any = "";
+  url:any = 'http://localhost:4200/assets/images/nd.png';
+  imgName:any = '';
+  onSelectFile(e:any){
+    //this.imgName = e.target.files[0];
+    var imgName = e.target.files[0];
+    if(e.target.files){
+    var reader =  new FileReader();
+    
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = (event:any)=>{
+          this.url = event.target.result;
+    }
+    }
+  }
   ngOnInit(): void {
     
   }
   addProduct(form:any){
     
     let products:any = {
-      'id':4,
+   
       'name':form.title,
       'description':form.desc,
       'rating': form.rating,
       'categoryId': form.allcats,
       'review':form.reviews,
       'vendor_name':form.vendors,
-      'image':'',
+      'image':`http://localhost:4200/assets/images/`,
       'is_available':form.is_available,
       'warranty':form.warranty  
     }
-    //console.log(products);
+     //console.log(products);
     
     this.proService.createProducts(products).subscribe(data => console.log(data));
   
